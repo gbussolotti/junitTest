7c165e17bfa26ef9c6b05e829afd862e0a29b0da
gbussolotti <83429547+gbussolotti@users.noreply.github.com>
2021-06-06 15:05:28 -0300

    Add RuleTest.java

---
+++ src/test/java/com/test/RuleTest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.test;
+
+import java.io.File;
+import java.io.IOException;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+/**
+ *
+ * @author gusta
+ */
+public class RuleTest {
+    
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+    
+    @Test
+    public void shouldCreateFileInTemporaryFolder() throws IOException{
+        File created = folder.newFile("file.txt");
+        Assert.assertTrue(created.isFile());
+        Assert.assertEquals(folder.getRoot(), created.getParentFile());
+    }
+   
+    
+}
