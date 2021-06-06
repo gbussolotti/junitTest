dbf2f8d6918c96c715541e122e8884913d719a28
gbussolotti <83429547+gbussolotti@users.noreply.github.com>
2021-06-06 14:50:16 -0300

    Initial Commit

---
+++ src/main/java/com/test/Calculadora.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.test;
+
+/**
+ *
+ * @author gusta
+ */
+public class Calculadora {
+
+    public int add(int value1, int value2) {
+        int result = value1+value2;
+        System.out.println("The result is: " + result);
+
+        return result;
+    }
+}
---
+++ src/test/java/com/test/CalculadoraTest.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.test;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author gusta
+ */
+public class CalculadoraTest {
+    
+    public CalculadoraTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of add method, of class Calculadora.
+     */
+    @Test
+    public void testAdd() {
+        System.out.println("add");
+        int value1 = 5;
+        int value2 = 5;
+        Calculadora instance = new Calculadora();
+        int expResult = 11;
+        int result = instance.add(value1, value2);
+        assertEquals(expResult, result);
+    }
+    
+}
